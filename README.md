737d6fb4154249fb5cdabbfdd0179e4f6d460bee
Milagros Zare <miluurosario13@gmail.com>
2024-12-03 23:08:48 -0300

    details + README

---
+++ README.md
@@ -0,0 +1,17 @@
+## TEMA SELECCIONADO: 
+Clon de Whatsapp Web. 
+
+## Librerías utilizadas: 
+React-router-dom
+React-Icons
+
+## Dificultades: 
+Si bien hay una parte del localStorage realizado, logré que se guarde en el almacenamiento, pero al momento de cerrar el navegador y volver a abrirlo, lo que no pude lograr es que se renderice lo que previamente se había guardado. Entiendo que es una oportunidad enorme de mejora, porque a pesar de haber buscado cómo realizarlo, no logré con ese objetivo.
+
+Luego, otro punto que no me explico por qué, es que al momento de actualizar la página, el header desaparece. Digamos que al recargar, vuelve a su estado "null", y no supe cómo corregirlo.
+
+Hay muchos detalles que me hubieran gustado agregar y mejorar, quizás mucha más funcionalidad en la página, en los botones. 
+
+## Feedback del trabajo y experiencia: 
+Para ser el primer trabajo realizado con React fue todo un desafío.
+Se aprende muchísimo más con la práctica, estancarnos y buscar maneras de avanzar. Me parece una buena actividad para comenzar, y si uno dispone de tiempo suficiente, lanzarse a Slack también es una gran oportunidad de aprendizaje.
--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -7,17 +7,14 @@ import HeaderChat from '../HeaderChat/HeaderChat'
 import NewMessage from '../NewMessage/NewMessage'
 
 const Chat = () => {
-  const {contact_id} = useParams()
+  const { contact_id } = useParams()
   const navigate = useNavigate()
 
   const { getContactById } = useContext(ContactsContext)
 
   const contact_selected = getContactById(Number(contact_id))
 
-  const messages_scroll = useRef(null) //messages_scroll: desplazamiento acompaña al último mensaje. 
-
-
-  useEffect(() => { //handleEscape
+  useEffect(() => {
     const handleEscape = (event) => {
       if (event.key === 'Escape') {
         navigate('/')
@@ -27,32 +24,25 @@ const Chat = () => {
     return () => window.removeEventListener('keydown', handleEscape)
   }, [])
 
-
-  useEffect(() => { // --EN REVISIÓN--
-    if (messages_scroll.current){
-      messages_scroll.current.scrollIntoView({ behavior: 'smooth'})
-    }
-  }, [contact_selected?.messages])
-
   return (
     <div className='chat-screen'>
-    {
-      !contact_selected
-      ? ''
-      : (<div className='chat'>
-        <div className='component-HeaderChat'>
-          <HeaderChat
-          user={contact_selected.name}
-          img={contact_selected.img} />
-        </div>
-        <div className="component-MessageList">
-        <MessageList
-        messages={contact_selected.messages}/>
-          </div>
-          <div className="component-NewMessage">
-            <NewMessage />
-          </div>
-      </div>)
+      {
+        !contact_selected
+          ? ''
+          : (<div className='chat'>
+            <div className='component-HeaderChat'>
+              <HeaderChat
+                user={contact_selected.name}
+                img={contact_selected.img} />
+            </div>
+            <div className="component-MessageList">
+              <MessageList
+                messages={contact_selected.messages} />
+            </div>
+            <div className="component-NewMessage">
+              <NewMessage />
+            </div>
+          </div>)
       }
     </div>
   )
